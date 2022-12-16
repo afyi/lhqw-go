904940dc87931aab01a4bd7d13a6d65acdb9e5f5
在下猫南北 <rakiy@qq.com>
2022-12-17 00:29:44 +0800

    模块化

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/afyi/lhwqcore
+module github.com/afyi/lhwq
 
 go 1.19
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	core "github.com/afyi/lhwqcore/core"
+	core "github.com/afyi/lhqw-go/core"
 )
 
 var Usage = func() {
